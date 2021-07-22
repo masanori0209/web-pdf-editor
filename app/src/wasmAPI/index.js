986a0f3e660b85769931d111d009cade0a793b06
masanori0209 <masanorimurakoshi0209@gmail.com>
2021-07-22 20:40:00 +0900

    rust add

--- app/src/wasmAPI/index.js
+++ app/src/wasmAPI/index.js
@@ -1,13 +1,8 @@
-let wasmAPI
-const loadWasm = (async (context) => {
-    const wasm = import("../../../../pkg")
-    wasmAPI = await wasm
-})
+const wasmAPI = import("../../../../pkg")
 const calcNewPoint = (async (context, payload) => {
-    if (wasmAPI == undefined) {
-        await context.dispatch('loadWasm')
-    }
-    const newPoint = await wasmAPI.calc_point(JSON.stringify(payload.settings), payload.angle)
+    const newPoint = await wasmAPI.calc_point(
+        JSON.stringify(payload.settings), payload.angle
+    )
     context.commit("setPoint", {
         point: JSON.parse(newPoint),
     })
