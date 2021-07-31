38b6f94972ba5aadfdefd189f1e85b04f0cfe2ee
masanori0209 <masanorimurakoshi0209@gmail.com>
2021-07-31 11:45:05 +0900

    struct add

--- app/wasm/src/lib.rs
+++ app/wasm/src/lib.rs
@@ -1,9 +1,6 @@
 mod utils;
-
 use wasm_bindgen::prelude::*;
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
@@ -14,12 +11,41 @@ extern {
 }
 
 #[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+
+    #[wasm_bindgen(js_namespace = console, js_name = log)]
+    fn log_u32(a: u32);
+
+    // Multiple arguments too!
+    #[wasm_bindgen(js_namespace = console, js_name = log)]
+    fn log_many(a: &str, b: &str);
+}
+
+macro_rules! console_log {
+    ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
+}
+
+#[wasm_bindgen]
 pub fn greet() {
     alert("Hello, web-pdf-editor!");
 }
 
 #[wasm_bindgen]
-pub fn file_upload_view(parameter: &JsValue) {
-    println!("parameter is {}", parameter)
-    0
+pub fn file_upload_view(a: JsValue) {
+    console_log!("parameter is {}", &parameter)
+}
+
+#[wasm_bindgen]
+pub struct FilePDF {
+    beforePdf: Vec<u8>,
+    afterPdf: Vec<u8>,
+}
+
+#[wasm_bindgen]
+impl FilePDF {
+    pub fn new(beforePdf: Vec<u8>, afterPdf: Vec<u8>) -> FilePDF {
+        FilePDF { beforePdf, afterPdf }
+    }
 }
